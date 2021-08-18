94927bdc0ece2ba10792fc58fc76e31fde2f4b3f
Chris <cmsl4y3r@gmail.com>
2021-08-17 19:48:02 -0600

    added favorites

--- apps/favorite_books_app/views.py
+++ apps/favorite_books_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from apps.login_registration_app.models import User
 from .models import Book
+from django.contrib import messages
 
 
 
@@ -23,6 +24,11 @@ def create_book(request):
         # TODO: add message after validation is running
         # messages.error(request, "You must log in to view this content.")
         return redirect('/')
+    errors = Book.objects.book_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request.value)
+        return redirect('/books')
     this_user = User.objects.filter(id=request.session['user_id'])[0]
     new_book = Book.objects.create(
         title = request.POST['title'],
@@ -63,14 +69,16 @@ def delete_book(request, book_id):
     this_book.delete()
     return redirect('/books/')
 
+# can remove by doing the opposite
 def like_book(request, book_id):
     if 'user_id' not in request.session and request.method != 'POST':
         # TODO: messages.error(request, "You must log in to view this content.")
         return redirect('/')
     this_book = Book.objects.filter(id=book_id)[0]
     this_user = User.objects.filter(id=request.session['user_id'])[0]
-    this_user.liked_books.add(this_book.id)
-    this_user.save() 
+    # this_user.liked_books.add(this_book.id)
+    this_book.users_who_like.add(this_user)
+    this_book.save() 
     return redirect('/books')
 
 def logout(request):
